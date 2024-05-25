c7a0efd1d3bc80223b246665f8b0c2585f7d5745
Hemanth <hemanthn2903@gmail.com>
2024-05-25 22:12:58 +0530

    Addition of Club Logo with the shuffling cards

--- src/components/home/layer2.jsx
+++ src/components/home/layer2.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import styles from "./layer2.module.css";
+import { useState, useEffect } from "react";
+import data from "./data";
+import { shuffle } from "lodash";
+import { useTransition, animated } from "@react-spring/web";
 
 export default function Layer2() {
-  return <></>;
+  const [rows, set] = useState(data);
+  useEffect(() => {
+    const t = setInterval(() => set(shuffle), 8000);
+    return () => clearInterval(t);
+  }, []);
+
+  let height = 0;
+  const transitions = useTransition(
+    rows.map((data) => ({ ...data, y: (height += data.height) - data.height })),
+    {
+      key: (item) => item.name,
+      from: { height: 0, opacity: 0 },
+      leave: { height: 0, opacity: 0 },
+      enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+      update: ({ y, height }) => ({ y, height }),
+    }
+  );
+  return (
+    <>
+      <div className={styles.list} style={{ height }}>
+        {transitions((style, item, t, index) => (
+          <animated.div
+            className={styles.card}
+            style={{ zIndex: data.length - index, ...style }}
+          >
+            <div className={styles.cell}>
+              <div
+                className={styles.details}
+                style={{ backgroundImage: item.css }}
+              >
+                {item.name}
+              </div>
+            </div>
+          </animated.div>
+        ))}
+      </div>
+    </>
+  );
 }
