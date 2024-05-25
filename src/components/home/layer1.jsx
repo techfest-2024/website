e04edbe927d0a04207c3da85a277fd1de312155a
Hemanth <hemanthn2903@gmail.com>
2024-05-25 19:01:50 +0530

    added files to watch

---
+++ src/components/home/layer1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./layer1.module.css";
+
+export default function Layer1() {
+  return (
+    <div className={styles.layer1__container}>
+      <h2 className={styles.title__main}>TechFest</h2>
+
+      <h4 className={styles.title__sub}>presented by GraVVIETy-24</h4>
+    </div>
+  );
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,69 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useRef } from "react";
+import ParallaxProps from "../components/home/parallaxProps";
+import Layer3 from "../components/home/layer3";
+import Layer2 from "../components/home/layer2";
+import Layer1 from "../components/home/layer1";
+
+export default function Home() {
+  const parallax = useRef(null);
+
+  return (
+    <div style={{ width: "100%", height: "100%", background: "#253237" }}>
+      <Parallax ref={parallax} pages={3}>
+        <ParallaxLayer
+          offset={1}
+          speed={1.5}
+          style={{ backgroundColor: "#805E73" }}
+        />
+        <ParallaxLayer
+          offset={2}
+          speed={0.5}
+          factor={1}
+          style={{ backgroundColor: "#87BC" }}
+        />
+
+        <ParallaxProps />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(1)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Layer1 />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Layer2 />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={() => parallax.current.scrollTo(0)}
+        >
+          <Layer3 />
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
