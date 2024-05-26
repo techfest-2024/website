9a8baaba4578497aebbc4a923f1a5fea10ad3a34
Sabah Sultana <sabahsultana918@gmail.com>
2024-05-26 21:25:41 +0530

    event page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Events from "./pages/events";
 import Home from "./pages/home";
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Events />
     </>
   );
 }
